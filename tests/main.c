69a36af869f8941d3b7065114a5f43a01addeaf1
Patryk Pujanek <163222150+nihiL7331@users.noreply.github.com>
2026-07-28 21:12:09 +0200

    feat: upward pass (#7)
    * feat: p2m->m2m pass test
    * feat: p2m->m2m pass implementation
    * fix: explicitly zero-out `ctx->root` on initialization
    * fix: check return value of `ps_calc_forces` in `test_fmm_upward_pass`
    * fix: start loop in `ps__sort_particles` from 1 to avoid underflow
    * refactor: allocate buffer for tests via `malloc` to ensure 16B alignment

--- tests/main.c
+++ tests/main.c
@@ -55,9 +55,11 @@ void test_morton_encoding(void) {
 }
 
 void test_arena_allocator(void) {
-    uint8_t    buffer[64];
+    void* buffer = malloc(64);
+    TEST_ASSERT(buffer != NULL, "Test buffer allocation failed");
+
     ps_arena_t arena;
-    ps_arena_init(&arena, buffer, sizeof(buffer));
+    ps_arena_init(&arena, buffer, 64);
 
     TEST_ASSERT(arena.cap == 64, "Arena capacity mismatch");
     TEST_ASSERT(arena.off == 0, "Arena offset should start at 0");
@@ -90,12 +92,16 @@ void test_arena_allocator(void) {
     TEST_ASSERT(
         ptr4 == ptr1,
         "4th allocation should reuse the same memory as 1st allocation");
+
+    free(buffer);
 }
 
 void test_octree_insertion(void) {
-    uint8_t       buffer[4096];
+    void* buffer = malloc(1024ULL * 4);
+    TEST_ASSERT(buffer != NULL, "Test buffer allocation failed");
+
     ps_context_t* ctx    = NULL;
-    ps_config_t   config = {buffer, sizeof(buffer)};
+    ps_config_t   config = {buffer, 1024ULL * 4};
 
     ps_result_t res = ps_init(&ctx, &config);
     TEST_ASSERT(res == PS_OK, "Failed to initialize context");
@@ -137,12 +143,16 @@ void test_octree_insertion(void) {
     TEST_ASSERT(curr->particle_cnt == 1, "Leaf particle count is wrong");
     TEST_ASSERT(curr->first_particle_idx == 99,
                 "Leaf stored wrong particle idx");
+
+    free(buffer);
 }
 
 void test_radix_sort(void) {
-    uint8_t       buffer[4096];
+    void* buffer = malloc(1024ULL * 4);
+    TEST_ASSERT(buffer != NULL, "Test buffer allocation failed");
+
     ps_context_t* ctx = NULL;
-    ps_config_t   cfg = {buffer, sizeof(buffer)};
+    ps_config_t   cfg = {buffer, 1024ULL * 4};
     ps_init(&ctx, &cfg);
 
     uint32_t morton_codes[4] = {999, 10, 500, 42};
@@ -166,12 +176,48 @@ void test_radix_sort(void) {
     TEST_ASSERT_FLOAT_EQ(4.0F, arrs.mass[1], 1e-6F);
     TEST_ASSERT_FLOAT_EQ(5.0F, arrs.mass[2], 1e-6F);
     TEST_ASSERT_FLOAT_EQ(9.0F, arrs.mass[3], 1e-6F);
+
+    free(buffer);
+}
+
+void test_fmm_upward_pass(void) {
+    void* buffer = malloc(1024ULL * 64);
+    TEST_ASSERT(buffer != NULL, "Test buffer allocation failed");
+
+    ps_context_t* ctx = NULL;
+    ps_config_t   cfg = {buffer, 1024ULL * 64};
+    ps_init(&ctx, &cfg);
+
+    float              x[2]    = {2.0F, -1.0F};
+    float              y[2]    = {3.0F, -2.0F};
+    float              z[2]    = {4.0F, -3.0F};
+    float              mass[2] = {2.0F, 3.0F};
+    ps_particle_arrs_t arrs    = {x, y, z, mass, NULL, NULL, NULL, 2};
+
+    ps_result_t res = ps_calc_forces(ctx, &arrs, 0.0F, 0.0F, 0.0F, 10.0F);
+    TEST_ASSERT(res == PS_OK, "Failed to calculate forces");
+
+    // M0 = sum(mass) = 2.0 + 3.0 = 5.0
+    // MX = sum(mass * (x - root_x)) = 2.0*2.0 + 3.0*-1.0 = 1.0
+    // MY = sum(mass * (y - root_y)) = 2.0*3.0 + 3.0*-2.0 = 0.0
+    // MZ = sum(mass * (z - root_z)) = 2.0*4.0 + 3.0*-3.0 = -1.0
+
+    ps_node_t* root = ctx->root;
+    TEST_ASSERT(root != NULL, "Root is null");
+
+    TEST_ASSERT_FLOAT_EQ(5.0F, root->multipole[0], 1e-4F);  // M0
+    TEST_ASSERT_FLOAT_EQ(1.0F, root->multipole[1], 1e-4F);  // MX
+    TEST_ASSERT_FLOAT_EQ(0.0F, root->multipole[2], 1e-4F);  // MY
+    TEST_ASSERT_FLOAT_EQ(-1.0F, root->multipole[3], 1e-4F); // MZ
+
+    free(buffer);
 }
 
 int main(void) {
     RUN_TEST(test_morton_encoding);
     RUN_TEST(test_arena_allocator);
     RUN_TEST(test_octree_insertion);
+    RUN_TEST(test_fmm_upward_pass);
 
     return 0;
 }
