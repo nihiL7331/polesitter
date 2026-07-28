fd9f20f94bf095319b0033f063726b3d40cbf73f
Patryk Pujanek <163222150+nihiL7331@users.noreply.github.com>
2026-07-28 03:41:22 +0200

    feat: morton encoding (#1)
    * feat: `ps__expand_bits` helper for `ps__morton_encode` impl
    * feat: `ps__morton_encode` implementation

--- src/polesitter.h
+++ src/polesitter.h
@@ -11,11 +11,28 @@
 
 #ifdef POLESITTER_IMPLEMENTATION
 
+// take a 10b num and expand it to 30b by inserting 2 0s between each b.
+static uint32_t ps__expand_bits(uint32_t v) {
+    v &= 0x000003FF; // only look at 10 ls bits
+
+    v = (v | (v << 16)) & 0x030000FF;
+    v = (v | (v << 8)) & 0x0300F00F;
+    v = (v | (v << 4)) & 0x030C30C3;
+    v = (v | (v << 2)) & 0x09249249;
+
+    return v;
+}
+
+// final number has 32 bits.
+// diving it by 3 dimensions, we can store 10 bits per dimension.
+// it interleaves the expanded bits of x, y and z.
 static uint32_t ps__morton_encode(uint32_t x, uint32_t y, uint32_t z) {
-    (void)x;
-    (void)y;
-    (void)z;
-    return 0;
+    uint32_t xx = ps__expand_bits(x);
+    uint32_t yy = ps__expand_bits(y);
+    uint32_t zz = ps__expand_bits(z);
+
+    // x takes bit 0, y shifts to bit 1, z shifts to bit 2
+    return xx | (yy << 1) | (zz << 2);
 }
 
 #endif // POLESITTER_IMPLEMENTATION
