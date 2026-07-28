388e69e655dd489de93e9f0b0e42d956267954c3
nihiL7331 <pujanekp6@gmail.com>
2026-07-28 03:38:22 +0200

    feat: `ps__expand_bits` helper for `ps__morton_encode` impl

--- src/polesitter.h
+++ src/polesitter.h
@@ -11,6 +11,18 @@
 
 #ifdef POLESITTER_IMPLEMENTATION
 
+// take a 10b num and expand it to 30b by inserting 2 0s between each b.
+static uint32_t ps__expand_bits(uint32_t v) {
+    v &= 0x000003FF; // only look at 10 ls bits
+
+    v = (v | (v << 16)) & 0x030000FF;
+    v = (v | (v << 8)) & 0x0300F00F;
+    v = (v | (v << 4)) & 0x030C30C3;
+    v = (v | (v << 2)) & 0x09249249;
+
+    return v;
+}
+
 static uint32_t ps__morton_encode(uint32_t x, uint32_t y, uint32_t z) {
     (void)x;
     (void)y;
